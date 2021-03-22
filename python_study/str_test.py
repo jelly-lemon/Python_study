cdb8ba696a9b47d4be5ea770f9adbbab7016e1d1
desktop lemon <lemeng1@qq.com>
2021-03-22 22:44:19 +0800

    daily commit

--- python_study/str_test.py
+++ python_study/str_test.py
@@ -1,4 +1,6 @@
+# -*- encoding:utf-8 -*-
 import json
+import sys
 
 
 def test_1():
@@ -22,31 +24,37 @@ def test_3():
     s ="abc"
     if s.find("bc") != -1:
         print("yes")
-        s = s.replace("bc", "aa")
-        print(s)
 
 def test_4():
     # 字符串转 dict
     config = '''{"company": "nsfocus", "phone": "%mobile%", "code": "验证码%code%"}'''
-    # config = json.loads(config)   # json里面的字符串都是unicode（见http://json.org/）
+    # 方法 1
+    # config = json.loads(config)   # json里面的字符串都是 unicode字符（见http://json.org/）
     # print(config)
+
+    # 方法2
     config = eval(config)
     print(config)
 
-def test_5():
-    s = u"abc%code%123"
-    s = s.replace("%code%", "验证码")
-    print(s)
 
-
-def test_6():
-    s1 = u"【nsfocus】有内鬼，停止交易！"
-    s1 = s1.encode("GBK")
-
-    s2 = "你好"
-    print(s2)
 
 if __name__ == '__main__':
-    s = "绿盟科技".encode("UTF-8")
+    print("默认编码方式：")
+    print(sys.getdefaultencoding())
+    print("控制台编码方式：")
+    print(sys.stdout.encoding)  # 打印stdout的编码方式
 
-    print(s)
+    s = "你好"
+
+
+
+
+
+
+
+
+
+
+
+
+
