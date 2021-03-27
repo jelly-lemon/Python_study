ec109ae3c60c2342f11da3b89d3232b6af7a0113
desktop lemon <lemeng1@qq.com>
2021-03-27 20:24:31 +0800

    daily commit

--- socket/server.py
+++ socket/server.py
@@ -1,14 +1,18 @@
 """
 socket 服务端
+
+
 """
 
 import socket
+import time
 
 if __name__ == '__main__':
     s = socket.socket()  # 创建 socket 对象
     host = socket.gethostname()  # 获取本地主机名
     port = 12345  # 设置监听端口
-    s.bind((host, port))
+    # s.bind((host, port))  # 如果用本地主机名，那么对方连接时也要用主机名才行
+    s.bind(("localhost", port))
     print("server %s:%s" % (host, port))
 
     s.listen(5)  # 最大监听数量
@@ -16,4 +20,5 @@ if __name__ == '__main__':
         conn, addr = s.accept()  # 接受连接
         print("客户端地址：%s" % str(addr))
         conn.send("欢迎访问服务端！".encode("utf-8"))  # 向客户端发送数据
+        time.sleep(1)
         conn.close()  # 关闭连接
