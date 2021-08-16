4ea2b1180c8f2978298e61000b3799ca2888c60d
nsfocus-lm <lemeng1@qq.com>
2021-08-16 13:46:30 +0800

    daily commit

--- double_click_open_file.py
+++ double_click_open_file.py
@@ -1,11 +1,19 @@
 """
-和双击打开文件一样，非阻塞
+模拟双击打开文件。
+
+和双击打开文件一样，非阻塞。
 
 只能用斜线，不能用双反斜线 D:/0-0-pycharm/Python_study/img/head.jpg
 """
 import os
 
-os.startfile("D:/0-0-pycharm/Python_study/img/head.jpg")
-print("1")
-os.startfile("D:/0-0-pycharm/Python_study/img/logo.png")
-print("2")
+
+def test_0():
+    os.startfile("D:/0-0-pycharm/Python_study/img/head.jpg")
+    print("1")
+    os.startfile("D:/0-0-pycharm/Python_study/img/logo.png")
+    print("2")
+
+
+if __name__ == '__main__':
+    test_0()
