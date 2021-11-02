45c0de11290b4529a50f7a0804d09bf4c36f60fc
b312-lemon <lemeng1@qq.com>
2021-11-02 18:07:07 +0800

    daily commit

--- plt_study/line_study.py
+++ plt_study/line_study.py
@@ -5,27 +5,15 @@ from math import log
 
 from matplotlib import pyplot as plt
 
-def draw_line(x:list, y:list) -> None:
-    """
-    折线图
-    """
-    plt.plot(x, y)
-    plt.show()
-
-def draw_some_lines(x:list, y:list) -> None:
-    """
-    多个折线图位于一个画布内
-    """
-
-
 
 def test_0():
     """
-    画折线图
+    折线图
     """
     x = [1,2,3,4,5,6,7]
     y = [0.5,0.6,0.7,0.8,0.9,0.8,0.7]
-    draw_line(x, y)
+    plt.plot(x, y)
+    plt.show()
 
 def test_1():
     """
@@ -35,7 +23,8 @@ def test_1():
     y = [1/(IR*log(IR, 2)) for IR in x]
     print(x)
     print(y)
-    draw_line(x, y)
+    plt.plot(x, y)
+    plt.show()
 
 if __name__ == '__main__':
     test_1()
--- plt_study/plt_study.py
+++ plt_study/plt_study.py
@@ -1,6 +1,6 @@
 """
 面向过程方式：
-
+plt 用到底
 
 面向对象方式：
 FigureCanvas（画布）、 Figure (图像)、 Axes (轴域)
@@ -31,9 +31,10 @@ def test_1():
             axes[i][j].yaxis.set_major_formatter(mtick.FormatStrFormatter('%.4f'))
 
     # 调整间距
-    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.2, hspace=0.2)       # wspace 水平间距
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.2, hspace=0.2)  # wspace 水平间距
     plt.suptitle("总标题", fontsize=16)
     plt.show()
+
 
 def test_2():
     """
@@ -49,5 +50,38 @@ def test_2():
     plt.show()
 
 
+def test_3():
+    """
+    面向对象方式画图
+    """
+    x = [1, 2, 3, 4, 5, 6, 7]
+    y = [0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.7]
+
+    # plt.plot(x, y) 是面向过程方式
+    fig, axis = plt.subplots(1)
+    axis.set_title("Hello World")
+    axis.plot(x, y)
+    fig.show()
+
+
+def test_4():
+    """
+    TODO 设置各种字体
+    """
+    x = [1, 2, 3, 4, 5, 6, 7]
+    y = [0.5, 0.6, 0.7, 0.8, 0.9, 0.8, 0.7]
+
+    my_font = {'family': 'Times New Roman',
+               'weight': 'bold',
+               'size': 24,
+               }
+
+    # plt.plot(x, y) 是面向过程方式
+    fig, axis = plt.subplots(1)
+    axis.set_title("Hello World", fontdict=my_font)
+
+    fig.show()
+
+
 if __name__ == '__main__':
-    test_2()
+    test_3()
